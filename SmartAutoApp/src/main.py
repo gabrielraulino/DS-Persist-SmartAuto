0493f44243732a2eb069f7c3c15b522fc88c3890
gabrielraulino <gabrielviana@alu.ufc.br>
2024-11-21 14:30:51 -0300

    refactor(main): organização dos imports

--- SmartAutoApp/src/main.py
+++ SmartAutoApp/src/main.py
@@ -1,5 +1,17 @@
+# imports do fastAPI
 from typing import Union
 from fastapi import FastAPI
+
+# imports dos models
+from pydantic import BaseModel
+from src.model.Endereco import Endereco
+from src.model.Veiculo import Veiculo
+from src.model.Venda import Venda
+from src.model.Locacao import Locacao
+from src.model.Cliente import Cliente
+from src.model.Vendedor import Vendedor
+from src.model.Gerente import Gerente
+from src.model.Cliente import Cliente
 
 app = FastAPI()
 
