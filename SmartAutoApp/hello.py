73a1b9f8e44b47832b8be222526af2c271e9189c
gabrielraulino <gabrielviana@alu.ufc.br>
2024-11-21 14:34:07 -0300

    test(hello): teste de criação das entidades com endereço

--- SmartAutoApp/hello.py
+++ SmartAutoApp/hello.py
@@ -1,6 +1,10 @@
 import uuid
+
 # from src.model.Vendedor import Vendedor
 from src.model.Endereco import Endereco
+from src.model.Cliente import Cliente
+
+
 def main():
     print("Hello from smartautoapp!")
 
@@ -11,14 +15,21 @@ if __name__ == "__main__":
 # vendedor = Vendedor(id=12312456, usuario="vendedor123", senha="senha_segura", nome="João Silva")
 # print(vendedor._id)
 endereco = Endereco(
-        id=uuid.uuid4(),
-        uf="CE",
-        cidade="Fortaleza",
-        logradouro="Rua das Flores",
-        numero="123"
-    )
+    id=uuid.uuid4(),
+    uf="CE",
+    cidade="Fortaleza",
+    logradouro="Rua das Flores",
+    numero="123",
+)
 # admin = Vendedor(id=uuid.uuid4(), usuario="admin123", senha="senha_admin", nome="Admin User")
 
 # print(admin)
-
+cliente = Cliente(
+    id=uuid.uuid4(),
+    nome="Gabriel Souza",
+    email="gabriel.souza@example.com",
+    telefone="123456789",
+    endereco=endereco,
+)
 print(endereco)
+print(cliente)
