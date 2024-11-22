f5ba252b4980fb15044f77e3379f0f72ce9e05cc
Antonio Kleberson <klebersonsilva862@gmail.com>
2024-11-22 15:21:16 -0300

    fix(models): renomeção classe funcionario

--- SmartAutoApp/src/routes/funcionarios.py
+++ SmartAutoApp/src/routes/funcionarios.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from uuid import UUID
 from typing import List
-from models.funcionario import Funcionario, Role
+from models.Funcionario import Funcionario, Role
 
 
 # from storage.file_handler import append_csv, read_csv
