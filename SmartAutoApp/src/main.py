2a34890a25b0918870374cd6071e350d3509c98a
Gabriel Raulino <gabrielviana@alu.ufc.br>
2024-11-22 10:15:12 -0300

    refactor(main): importação rotas

--- SmartAutoApp/src/main.py
+++ SmartAutoApp/src/main.py
@@ -1,56 +1,37 @@
 # imports do fastAPI
+from http import HTTPStatus
 from typing import Union
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from routes.funcionarios import funcionarios_router
+from models.cliente import Cliente
+from models.endereco import Endereco
+
+from routes.clientes import clientes_router
+
+# from src.routes.funcionario_route import fun
 import uuid
 
-from model.Cliente import Cliente
-
-from model.Funcionario import Funcionario
-from model.Endereco import Endereco
-
-
-vendedores: List[Funcionario] = [
-    {
-        "id": 1,
-        "usuario": "beulah_g",
-        "senha": "senha_segura",
-        "telefone": "(526) 880-5527",
-    },
-    {
-        "id": 2,
-        "usuario": "john_d",
-        "senha": "senha_forte",
-        "telefone": "(123) 456-7890",
-    },
-    {
-        "id": 3,
-        "usuario": "jane_s",
-        "senha": "senha_segura123",
-        "telefone": "(987) 654-3210",
-    },
-]
-
-
-endereco = Endereco(
-    id=uuid.uuid4(),
-    uf="CE",
-    cidade="Fortaleza",
-    logradouro="Rua das Flores",
-    numero="123",
-)
-clientes: List[Cliente] = [
-    {
-        "id": uuid.uuid1(),
-        "nome": "joao",
-        "telefone": "998876654",
-        "email": "example@domain.com",
-        "endereco": endereco,
-    }
-]
 
 app = FastAPI()
+# endereco = Endereco(
+#     id=uuid.uuid4(),
+#     uf="CE",
+#     cidade="Fortaleza",
+#     logradouro="Rua das Flores",
+#     numero="123",
+# )
+
+# clientes: List[dict] = [
+#     {
+#         "id": uuid.uuid1(),
+#         "nome": "joao",
+#         "telefone": "998876654",
+#         "email": "example@domain.com",
+#         "endereco": endereco,
+#     }
+# ]
 
 
 @app.get("/")
@@ -58,11 +39,5 @@ def read_root():
     return {"msg": "Hello World"}
 
 
-@app.get("/vendedores/")
-def listar_vendedores():
-    return vendedores
-
-
-@app.get("/clientes/")
-def listar_clientes():
-    return clientes
+app.include_router(funcionarios_router)
+app.include_router(clientes_router)
