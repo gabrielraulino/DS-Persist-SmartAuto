705570a6bc332a37e2e79f98457f9e8f5d5ccf4d
Gabriel Raulino <gabrielviana@alu.ufc.br>
2024-11-23 16:49:54 -0300

    refactor(main): adicionar informações de autores e comentar inclusão do rota de funcionários

--- SmartAutoApp/src/main.py
+++ SmartAutoApp/src/main.py
@@ -1,17 +1,13 @@
+"""
+Autores : Gabriel Raulino, Antonio Kleberson 
+"""
+
 # imports do fastAPI
-from http import HTTPStatus
-from typing import Union
-from typing import List
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 from routes.funcionarios import funcionarios_router
-from models.cliente import Cliente
-from models.endereco import Endereco
+
 
 from routes.clientes import clientes_router
-
-# from src.routes.funcionario_route import fun
-import uuid
 
 
 app = FastAPI()
@@ -22,5 +18,5 @@ def read_root():
     return {"msg": "SmartAutoApp"}
 
 
-app.include_router(funcionarios_router)
+# app.include_router(funcionarios_router)
 app.include_router(clientes_router)
