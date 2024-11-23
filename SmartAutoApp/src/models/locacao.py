ac9b9093963e5504ed786a7b9d6803c86ba2903e
Antonio Kleberson <klebersonsilva862@gmail.com>
2024-11-23 14:09:35 -0300

    feat(locacao): Implementação da classe locacao

--- SmartAutoApp/src/models/locacao.py
+++ SmartAutoApp/src/models/locacao.py
@@ -1,6 +1,15 @@
-from pydantic import BaseModel
+#Autor: Antonio Kleberson
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import date
 import uuid
 
-
 class Locacao(BaseModel):
-    id: int
+    id: uuid.UUID = Field(default_factory=uuid.uuid4)
+    data_inicio: date
+    data_fim: date
+    valor_diaria: float
+    cliente_id: uuid.UUID
+    vendedor_id: uuid.UUID
+    veiculo_id: uuid.UUID
+
