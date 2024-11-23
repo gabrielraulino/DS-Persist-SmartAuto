56b7a71560b06665db8d687f813aafb6e4845f9c
Gabriel Raulino <gabrielviana@alu.ufc.br>
2024-11-23 19:28:47 -0300

    refactor(zip_handler): melhorar a geração do nome do arquivo ZIP ao compactar CSV

--- SmartAutoApp/src/utils/zip_handler.py
+++ SmartAutoApp/src/utils/zip_handler.py
@@ -1,6 +1,7 @@
 import zipfile
 import os
 
+
 def compactar_csv(file_path: str) -> str:
     """
     Compacta um arquivo CSV em um arquivo ZIP.
@@ -12,11 +13,13 @@ def compactar_csv(file_path: str) -> str:
         str: Caminho do arquivo ZIP gerado.
     """
     # Define o nome do arquivo ZIP com base no CSV
-    zip_file_path = f"{file_path}.zip"
-    
+    zip_file_path = file_path.replace(
+        ".csv", ".zip"
+    )  # garante que o nome do arquivo fique com o final .zip
+
     # Cria o arquivo ZIP
     with zipfile.ZipFile(zip_file_path, "w", zipfile.ZIP_DEFLATED) as zip_file:
         # Adiciona o arquivo CSV ao ZIP
         zip_file.write(file_path, os.path.basename(file_path))
-    
+
     return zip_file_path
