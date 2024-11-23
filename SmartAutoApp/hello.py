87f2428a4b8a75520ca70d27e50daf8e8f2df4e8
Gabriel Raulino <gabrielviana@alu.ufc.br>
2024-11-23 00:14:02 -0300

    feat(cliente): teste de implementação lógica para criar e exibir DataFrame de clientes

--- SmartAutoApp/hello.py
+++ SmartAutoApp/hello.py
@@ -19,7 +19,6 @@ try:
     df = pd.read_csv(cliente_csv)
 except Exception as e:
     df = pd.DataFrame()
-
 endereco = Endereco(
     id=uuid.uuid4(),
     uf="CE",
@@ -36,7 +35,21 @@ cliente = Cliente(
     telefone="123456789",
     endereco=endereco,
 )
-
-
-print(endereco)
-print(cliente)
+clientes_df = pd.DataFrame(columns=["id", "nome", "telefone", "email", "endereco"])
+novo_cliente = pd.DataFrame(
+    [
+        {
+            "id": cliente.id,
+            "nome": cliente.nome,
+            "telefone": cliente.telefone,
+            "email": cliente.email,
+            "endereco": f"{cliente.endereco.logradouro}, {cliente.endereco.numero}, {cliente.endereco.cidade} - {cliente.endereco.uf}",
+        }
+    ]
+)
+# clientes_df.append(cliente)
+clientes_df = pd.concat([clientes_df, novo_cliente])
+# df.to_csv(cliente_csv)
+print(clientes_df)
+# print(endereco)
+# print(cliente)
