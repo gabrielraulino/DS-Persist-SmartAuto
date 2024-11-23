133ae9249226589227570314b2d746b991c4921d
Gabriel Raulino <gabrielviana@alu.ufc.br>
2024-11-23 19:46:25 -0300

    refactor(main): reorganizar importações e incluir novos routers para vendas e locações

--- SmartAutoApp/src/main.py
+++ SmartAutoApp/src/main.py
@@ -3,12 +3,12 @@ Autores : Gabriel Raulino, Antonio Kleberson
 """
 
 # imports do fastAPI
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from routes.funcionarios import funcionarios_router
-
-
 from routes.clientes import clientes_router
-
+from routes.veiculos import veiculos_router
+from routes.vendas import vendas_router
+from routes.locacoes import locacoes_router
 
 app = FastAPI()
 
@@ -18,5 +18,7 @@ def read_root():
     return {"msg": "SmartAutoApp"}
 
 
-# app.include_router(funcionarios_router)
-app.include_router(clientes_router)
+app.include_router(funcionarios_router)
+# app.include_router(clientes_router)
+# app.include_router(veiculos_router)
+app.include_router(vendas_router)
