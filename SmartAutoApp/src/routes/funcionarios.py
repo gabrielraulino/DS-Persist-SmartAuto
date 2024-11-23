727f58fd86e741339e96d23cde0c3014c9f30b2e
Gabriel Raulino <gabrielviana@alu.ufc.br>
2024-11-23 14:21:47 -0300

    feat(funcionarios): integrar leitura e escrita de dados de funcionários em CSV

--- SmartAutoApp/src/routes/funcionarios.py
+++ SmartAutoApp/src/routes/funcionarios.py
@@ -2,15 +2,15 @@ from http import HTTPStatus
 from fastapi import APIRouter, HTTPException
 import uuid
 from typing import List
-from SmartAutoApp.src.models.funcionario import Funcionario, Role
-
-
-# from storage.file_handler import append_csv, read_csv
-import uuid
+from models.funcionario import Funcionario, Role
+from storage.file_handler import read_csv, append_csv, write_csv
 
 funcionarios_router = APIRouter()
+file = "src/storage/funcionarios.csv"
+campos = ["id", "usuario", "senha", "nome", "telefone", "funcao"]
 
-funcionarios: List[Funcionario] = []
+funcionarios_data = read_csv(file)
+funcionarios: List[Funcionario] = [Funcionario(**f) for f in funcionarios_data]
 
 
 @funcionarios_router.get("/funcionarios/")
@@ -22,22 +22,30 @@ def listar_clientes():
     "/funcionarios/", response_model=Funcionario, status_code=HTTPStatus.CREATED
 )
 def inserir(funcionario: Funcionario):
-    if funcionario.endereco.id == None:
-        funcionario.endereco.id = uuid.uuid4()
+    try:
+        role = Role(funcionario.funcao.lower())
+    except ValueError:
+        raise HTTPException(
+            status_code=400,
+            detail="Função inválida. Valores permitidos: 'vendedor', 'gerente', 'admin'.",
+        )
+    funcionario.funcao = role
     if funcionario.id == None:
         funcionario.id = uuid.uuid4()
     elif any(f.id == funcionario.id for f in funcionarios):
         raise HTTPException(status_code=400, detail="ID já existe.")
     # Verifica se id de funcionario e endereco é não nulo
     funcionarios.append(funcionario)
+    append_csv(file, campos, funcionario.model_dump())
     return funcionario
 
 
 @funcionarios_router.get("/funcionarios/{id}")
 def buscar(id: uuid.UUID):
-    for indice, cliente_atual in enumerate(funcionarios):
-        if cliente_atual.id == id:
-            return cliente_atual
+    # for cliente_atual in funcionarios:
+    for f in funcionarios:
+        if f.id == id:
+            return f
     raise HTTPException(
         status_code=HTTPStatus.NOT_FOUND, detail="Funcionário não encontrado."
     )
@@ -50,6 +58,9 @@ def atualizar(id: uuid.UUID, atualizado: Funcionario):
             if atualizado.id != id:
                 atualizado.id = id
             funcionarios[index] = atualizado
+            write_csv(
+                file, campos, [funcionario.model_dump() for funcionario in funcionarios]
+            )
             return atualizado
     raise HTTPException(
         status_code=HTTPStatus.NOT_FOUND, detail="Funcionário não encontrado."
@@ -61,6 +72,9 @@ def remover(id: uuid.UUID):
     for f in funcionarios:
         if f.id == id:
             funcionarios.remove(f)
+            write_csv(
+                file, campos, [funcionario.model_dump() for funcionario in funcionarios]
+            )
             return {"msg": "cliente removido com sucesso!"}
     raise HTTPException(
         status_code=HTTPStatus.NOT_FOUND, detail="Funcionário não encontrado."
