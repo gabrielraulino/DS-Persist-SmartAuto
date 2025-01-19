ef6a0c3a42a88fbd943ed831cff4064e3422975b
Antonio Kleberson <klebersonsilva862@gmail.com>
2025-01-19 18:43:27 -0300

    feat(models): adicionar relacionamento entre categorias e veículos

--- SmartAutoApp/src/models/categoria.py
+++ SmartAutoApp/src/models/categoria.py
@@ -1,12 +1,19 @@
 # Autor: Antonio Kleberson
-from sqlmodel import SQLModel, Field
-
-
-class Categoria(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    nome: str
-    desc: str
+from sqlmodel import SQLModel, Field, Relationship
+from typing import List, Optional
 
 class CategoriaVeiculo(SQLModel, table=True):
-    categoria_id = Field(default=None, foreign_key="categoria.id", primary_key=True)
-    veiculo_id = Field(default=None, foreign_key="veiculo.id", primary_key=True)
+    categoria_id: Optional[int] = Field(default=None, foreign_key="categoria.id", primary_key=True)
+    veiculo_id: Optional[int] = Field(default=None, foreign_key="veiculo.id", primary_key=True)
+
+class Categoria(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    nome: str
+    desc: str
+    veiculos: List["Veiculo"] = Relationship(back_populates="categorias", link_model=CategoriaVeiculo)
+
+class Veiculo(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    nome: str
+    modelo: str
+    categorias: List[Categoria] = Relationship(back_populates="veiculos", link_model=CategoriaVeiculo)
